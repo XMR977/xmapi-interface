c911ec15fa7a3738d77e72873128c086cdb9e484
xmr <yonglin1997@gmail.com>
2024-03-03 18:36:58 -0500

    API interface test with API-Client-SDK

---
+++ src/main/java/com/xmr/xmapiinterface/controller/NameController.java
@@ -0,0 +1,41 @@
+package com.xmr.xmapiinterface.controller;
+
+import jakarta.servlet.http.HttpServletRequest;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.web.bind.annotation.*;
+import com.xmr.xmapicilentsdk.model.User;
+
+/**
+ * Name API
+ */
+@RestController
+@RequestMapping("/name")
+@Slf4j
+public class NameController {
+
+    @GetMapping("/")
+    public String getNameByGet(String name){
+
+        return name;
+    }
+
+    @PostMapping("/")
+    public String getNameByPost(@RequestParam String name){
+        return name;
+    }
+
+    @PostMapping("/user")
+    public String getUsernameByPost(@RequestBody User user, HttpServletRequest httpServletRequest){
+        String accessKey = httpServletRequest.getHeader("accessKey");
+        String nonce = httpServletRequest.getHeader("accessKey");
+        String timestamp = httpServletRequest.getHeader("secretKey");
+        String sign = httpServletRequest.getHeader("accessKey");
+        String body = httpServletRequest.getHeader("secretKey");
+        if(!accessKey.equals("xm")){
+            throw  new RuntimeException("error");
+        }
+
+
+        return user.getUsername();
+    }
+}
--- src/test/java/com/xmr/xmapiinterface/XmapiInterfaceApplicationTests.java
+++ src/test/java/com/xmr/xmapiinterface/XmapiInterfaceApplicationTests.java
@@ -1,13 +1,32 @@
 package com.xmr.xmapiinterface;
 
+
+import com.xmr.xmapicilentsdk.client.XmApiClient;
+import com.xmr.xmapicilentsdk.model.User;
+
+import jakarta.annotation.Resource;
 import org.junit.jupiter.api.Test;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import com.xmr.xmapicilentsdk.XmApiClientConfig;
+import org.springframework.context.annotation.Import;
 
 @SpringBootTest
+@Import(XmApiClientConfig.class)
 class XmapiInterfaceApplicationTests {
 
+   @Resource
+   private XmApiClient xmApiClient;
     @Test
     void contextLoads() {
+        String xm = xmApiClient.getNameByGet("xm");
+        System.out.println(xm);
+        User user = new User();
+        user.setUsername("xm");
+        String usernmameByPost = xmApiClient.getUsernameByPost(user);
+        System.out.println(xm);
+        System.out.println(usernmameByPost);
     }
 
 }
