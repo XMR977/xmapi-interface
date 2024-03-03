a0bc8d5fd364b92eec6d92843b1470bcd783cb78
xmr <yonglin1997@gmail.com>
2024-03-03 18:35:56 -0500

    API interface and applied with API SDK Starter

---
+++ src/main/java/com/xmr/xmapiinterface/XmapiInterfaceApplication.java
@@ -0,0 +1,13 @@
+package com.xmr.xmapiinterface;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class XmapiInterfaceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(XmapiInterfaceApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/xmr/xmapiinterface/XmapiInterfaceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.xmr.xmapiinterface;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class XmapiInterfaceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
